a4d9e52ccd4a06e41fa4a65f0c881aecf6cf6ac7
MujaffarYadawad <mujaffaryadawad587313@gmail.com>
2023-01-31 10:57:27 +0530

    changes

--- controllers/premiumFeature.js
+++ controllers/premiumFeature.js
@@ -5,14 +5,7 @@ const sequelize = require("../util/database");
 exports.getUserLeaderBoard = async (req, res) => {
   try {
     const leaderboardusers = await User.findAll({
-      attributes: [
-        "id",
-        "name",
-        [
-          sequelize.fn("sum", sequelize.col("expenses.expenseAmount")),
-          "total_cost",
-        ],
-      ],
+      attributes: [ "id", "name",   [  sequelize.fn("sum", sequelize.col("expenses.expenseAmount")),  "total_cost",  ] ],
       include: [
         {
           model: Expense,
@@ -22,34 +15,6 @@ exports.getUserLeaderBoard = async (req, res) => {
       group: ["user.id"],
       order: [["total_cost", "DESC"]],
     });
-    
-    // const expenses = await Expense.findAll({
-    //   attributes : ['userId', [sequelize.fn('sum', sequelize.col('expenses.expenseAmount')), 'total_cost']],
-    //   group: ['userId'],
-    //   order: [['total_cost', 'DESC']]
-    // });
-    //const userAggregatedExpenses = {}
-    //console.log('user-->', users)
-    //console.log("expense-->", expenses);
-    // expenses.forEach((expense) => {
-    //   if (userAggregatedExpenses[expense.userId]) {
-    //     userAggregatedExpenses[expense.userId] =
-    //       userAggregatedExpenses[expense.userId] + expense.expenseAmount;
-    //     // console.log("y1");
-    //   } else {
-    //     userAggregatedExpenses[expense.userId] = expense.expenseAmount;
-    //     //console.log("y");
-    //   }
-    // })
-    
-      // var userLeaderBoardDetails = [];
-      // users.forEach((user) => {
-      //   userLeaderBoardDetails.push({name: user.name, total_cost: userAggregatedExpenses[user.id] || 0})
-      // })
-      
-      // //console.log(userLeaderBoardDetails)
-      // userLeaderBoardDetails.sort((a, b) => b.total_cost - a.total_cost)
-      // //console.log('user aggregate->>',userAggregatedExpenses)
 
       res.status(200).json(leaderboardusers);
    
