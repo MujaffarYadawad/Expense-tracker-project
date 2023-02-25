aff23ebe4965a6dd9ba7f211105f5fd1926a3518
MujaffarYadawad <mujaffaryadawad587313@gmail.com>
2023-02-25 11:26:15 +0530

    backend ready to toploy

--- public/forgotpassword/forgotpassward.js
+++ public/forgotpassword/forgotpassward.js
@@ -12,7 +12,7 @@ async function forgotpassword(event) {
 
   try {
     const res = await axios.post(
-      "http://54.95.147.222:3000/password/forgotpassword",
+      "http://localhost:3000/password/forgotpassword",
       userDetails
     );
 
